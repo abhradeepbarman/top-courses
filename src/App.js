6f9a2a86396d5d8194301c2ebd3e6b0573ef23ee
abhradeepbarman <abhradeep709@gmail.com>
2024-02-24 00:48:26 +0530

    small functionalities added

--- src/App.js
+++ src/App.js
@@ -3,35 +3,51 @@ import Navbar from './components/Navbar'
 import Filter from './components/Filter'
 import Cards from './components/Cards'
 import {filterData, apiUrl} from "./data"
-import {toast} from "react-toastify"
+import { toast  } from "react-toastify"
+import Spinner from './components/Spinner'
 
 const App = () => {
 
-  let [courses, setCourses] = useState('');
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [category, setCategory] = useState(filterData[0].title)
+
+  const fetchData = async () => {
+    setLoading(true);
+    try{
+      const res = await fetch(apiUrl);
+      const output = await res.json();
+
+      //save data into a variable
+      setCourses(output.data);
+    }
+    catch(err) {
+      toast.error("Something went wrong!");
+    }
+    setLoading(false);
+  }
 
   useEffect(() => {
-    const fetchData = async () => {
-      try{
-        const res = await fetch(apiUrl);
-        const output = await res.json();
-
-        //save data into a variable
-        setCourses(output.data);
-      }
-      catch(err) {
-        toast.error("Something went wrong!");
-      }
-    }
-
     fetchData();
   }, [])
 
   return (
-    <>
-      <Navbar />
-      <Filter filterData={filterData} />
-      <Cards courses={courses} />
-    </>
+    <div className="min-h-screen flex flex-col bg-bgDark2">
+      <div>
+        <Navbar />
+      </div>
+
+      <div className="bg-bgDark2">
+        <div>
+          <Filter filterData={filterData} category={category} setCategory={setCategory} />
+        </div>
+        <div className="w-11/12 max-w-[1200px] mx-auto flex flex-wrap justify-center items-center min-h-[50vh] ">
+          {
+            loading ? (<Spinner/>) : (<Cards courses={courses} category={category} />)
+          }
+        </div>
+      </div> 
+    </div>
   )
 };
 
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,25 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './Card'
 
-const Cards = ({courses}) => {
+const Cards = ({courses, category}) => {
+
+    const [likedCourses, setLikedCourses] = useState([]);
 
     let allCourses = [];
 
     //returns list of all courses
     const getCourses = () => {
-        Object.values(courses).forEach( (courseCategory) => {
-            courseCategory.forEach((course) => {
-                allCourses.push(course);
-            })  
-        })
-        return allCourses;
+        if(category === "All") {
+            Object.values(courses).forEach( (courseCategory) => {
+                courseCategory.forEach((course) => {
+                    allCourses.push(course);
+                })  
+            })
+            return allCourses;
+        }
+        else {
+            return courses[category];
+        }
     }
 
   return (
-    <div>
+    <div className='flex flex-wrap justify-center gap-4 mb-4'>
     {
         getCourses().map((course) => {
-            return <Card course={course} />;
+            return <Card key={course.id} course={course} likedCourses={likedCourses} setLikedCourses={setLikedCourses}  />;
         })
     }
     </div>
